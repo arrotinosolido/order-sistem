c6fb4635b6c0e94abe716fe48a4084c56cf7abc8
arrotinosolido <131671787+arrotinosolido@users.noreply.github.com>
2026-04-26 16:43:30 +0300

    Update home route response message

--- web.py
+++ web.py
@@ -1,12 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "WEB OK - running"
+    return "OK - Railway Web Running"
 
 if __name__ == "__main__":
-    import os
     port = int(os.getenv("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
