bab90bfd1dca5950ec314ff9e962a6aa309e8c73
arrotinosolido <131671787+arrotinosolido@users.noreply.github.com>
2026-04-26 15:38:58 +0300

    fix: add main.py to run Flask and aiogram bot concurrently

--- web.py
+++ web.py
@@ -54,4 +54,5 @@ def ready(oid):
     return {'ok': True}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
